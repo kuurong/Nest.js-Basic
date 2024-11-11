82e9310ac5ef66173009ecf135000be5f353cd2f
kuurong <s10y06@naver.com>
2024-11-11 16:20:44 +0900

    course - 121

--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -11,7 +11,7 @@ import { GetUsersParamDto } from '../dtos/getUsersParam.dto';
 import { AuthService } from 'src/auth/providers/auth.service';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../user.entity';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { CreateUserDto } from '../dtos/createUser.dto';
 import { ConfigService, ConfigType } from '@nestjs/config';
 import profileConfig from '../config/profile.config';
@@ -33,6 +33,9 @@ export class UsersService {
 
     @Inject(profileConfig.KEY)
     private readonly profileConfiguration: ConfigType<typeof profileConfig>,
+
+    // Injecting Datasource
+    private readonly dataSource: DataSource,
   ) {}
 
   public async createUser(createUserDto: CreateUserDto) {
@@ -120,6 +123,35 @@ export class UsersService {
     }
 
     return user;
+  }
+
+  public async createMany(createUsersDtos: CreateUserDto[]) {
+    let newUsers: User[] = [];
+    // Create Query Runner Instance
+    const queryRunner = this.dataSource.createQueryRunner();
+
+    // Connect Query Runner to datasource
+    await queryRunner.connect();
+
+    // Start Transactions
+    await queryRunner.startTransaction();
+    try {
+      for (let user of createUsersDtos) {
+        let newUser = queryRunner.manager.create(User, user);
+        let result = await queryRunner.manager.save(newUser); //한 user db 저장
+        newUsers.push(result);
+      }
+
+      // 성공적으로 commit
+      // with this line of code, we have successfully catered to the success condition.
+      await queryRunner.commitTransaction();
+    } catch (err) {
+      // 하나라도 실패면 rollback
+      await queryRunner.rollbackTransaction();
+    } finally {
+      // 성공이든 실패든 이제 DB연결해제
+      await queryRunner.release();
+    }
   }
 
   public async test() {
